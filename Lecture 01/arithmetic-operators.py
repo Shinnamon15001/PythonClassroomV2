97924f3182bd6ccd2d62bca7b613072013ba3ba3
Shinnamon15001 <s6606022610030@email.kmutnb.ac.th>
2023-12-06 18:40:14 +0700

    try split input hahah

---
+++ Lecture 01/arithmetic-operators.py
@@ -0,0 +1,5 @@
+# first_name, last_name = input("\nType your First name and last name here : ").split(' ', 1)
+# print("\nFirst name :", first_name, "\nLast name :", last_name, "\n")
+
+name, age, income = input("Enter your name : "), int(input("Enter your age : ")), float(input("Enter your income : "))
+print("Here is the data you entered\n", "Name :", name, "\nAge :", age, "\nIncome ", income)
