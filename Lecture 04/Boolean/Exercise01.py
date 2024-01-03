3c330dd65b55afeb064184ca2ac096ef798836b6
Shinnamon15001 <s6606022610030@email.kmutnb.ac.th>
2024-01-03 19:21:23 +0700

    update

---
+++ Lecture 04/Boolean/Exercise01.py
@@ -0,0 +1,14 @@
+words = input("Enter a string : ")
+
+if (words.isalnum()) != (words.isalpha()):
+    print("string isalphanumeric characters")
+if words.isalpha():
+    print("string is alphabetic characters")
+if words.islower():
+    print("string is lowercase")
+if words.isnumeric():
+    print("string is numeric characters")
+if words.istitle():
+    print("string is title case")
+if words.isupper():
+    print("string is upper case")
